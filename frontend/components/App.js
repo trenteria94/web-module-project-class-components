2bd1bcb85ba23ef965db521df2f4ecfdcac3151d
Timothy Renteria <Timothyxrenteria@gmail.com>
2024-03-31 21:06:14 -0700

    completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,44 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import Form from './Form'
+import Todo from './Todo'
+import TodoList from './TodoList'
 
+let id = 0
+let getId = () => ++id
+const intitalTodos = [
+  {id: getId(), name: 'Feed the cat', completed: false },
+  {id: getId(), name: 'Code for an hour', completed: true },
+  {id: getId(), name: 'Do laundry', completed: false }
+]
 export default class App extends React.Component {
+  state = {
+    todos: intitalTodos
+  }
+
+  addTodo = (name) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat({id: getId(), completed: false, name})
+    })
+  }
+  toggleCompletion = id => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(td => {
+        if (id == td.id) {
+          return {...td, completed: !td.completed}
+        }
+        return td
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todos={this.state.todos} toggleCompletion={this.toggleCompletion}/>
+        <Form addTodo={this.addTodo}/>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault()
+    this.props.addTodo(this.state.name)
+    this.setState({
+      name: ''
+    })
+
+  }
+
+  onChange = evt => {
+    const { value } = evt.target
+    this.setState({
+      name: value
+    })
+  }
+
   render() {
+
     return (
       <div>
-        Form
+        <form onSubmit={this.onSubmit}>
+          <input type='text' value={this.state.name} onChange={this.onChange} />
+          <input type='submit' />
+        </form>
       </div>
     )
   }
